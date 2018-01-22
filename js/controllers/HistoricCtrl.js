2ad0583937b1094a7fc451ca327a9f78eb3a281d
devil-geek <devil.prowler@gmail.com>
2018-01-21 21:00:42 -0600

    Update history view

--- js/controllers/HistoricCtrl.js
+++ js/controllers/HistoricCtrl.js
@@ -4,4 +4,34 @@ angular.module("app.controllers")
   $scope.$watch('startDate', function (value) {
      console.log(value)
   });
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['PH', 'Dissolved Oxigen', 'Turbidity'];
+  $scope.data = [
+    [5, 9, 8, 8.1, 6, 5, 4],
+    [28, 48, 40, 19, 86, 27, 90],
+    [38, 18, 20, 9, 56, 17, 70]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
 });
